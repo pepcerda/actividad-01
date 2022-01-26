f226995e8623b2559347a6d4efaf42085f1e6963
pepcerda <josep.cerda.roscar@gmail.com>
2022-01-26 21:45:14 +0100

    Creación de la clase C

--- src/index.js
+++ src/index.js
@@ -44,3 +44,54 @@ const leerValoryMostrar = () => {
     instanciaB.mostrarAlerta();  
 }
 
+class C {
+    constructor(num1, num2) {
+        this.num1=num1; 
+        this.num2=num2; 
+    }
+
+    suma() {
+        return this.num1 + this.num2; 
+    }
+
+    resta() {
+        return this.num1 - this.num2; 
+    }
+
+    multiplica() {
+        return this.num1 * this.num2; 
+    }
+}
+
+const sumar = () => {
+    let num1 = document.getElementById("num1"); 
+    let num2 = document.getElementById("num2"); 
+    let instanciaC = new C(num1.value, num2.value); 
+    let salida = document.createElement("INPUT"); 
+    salida.setAttribute("type", "number"); 
+    salida.setAttribute("value", Number(instanciaC.suma())); 
+    document.body.appendChild(salida); 
+}
+
+const restar = () => {
+    let num1 = document.getElementById("num1"); 
+    let num2 = document.getElementById("num2"); 
+    let instanciaC = new C(num1.value, num2.value);  
+    let salida = document.createElement("INPUT"); 
+    salida.setAttribute("type", "number");
+    salida.setAttribute("value", instanciaC.resta()); 
+    document.body.appendChild(salida); 
+}
+
+const multiplicar = () => {
+    let num1 = document.getElementById("num1"); 
+    let num2 = document.getElementById("num2"); 
+    let instanciaC = new C(num1.value, num2.value); 
+    let salida = document.createElement("INPUT"); 
+    salida.setAttribute("type", "number");
+    salida.setAttribute("value", instanciaC.multiplica()); 
+    document.body.appendChild(salida); 
+}
+
+
+
